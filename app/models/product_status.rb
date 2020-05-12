99b26a4874b3341651726439d6dd81127ce4f677
kashiwagi1096 <toguro.dayo@gmail.com>
2020-05-12 18:42:24 +0900

    修正

--- app/models/product_status.rb
+++ app/models/product_status.rb
@@ -1,10 +1,9 @@
 class Product_Status < ActiveHash::Base
   self.data = [
-      {id: 1, product_status: '選択してください'}, 
-      {id: 2, product_status: '新品未使用'},
-      {id: 3, product_status: '未使用に近い'},
-      {id: 4, product_status: '目立った傷や汚れなし'},
-      {id: 5, product_status: 'やや傷や汚れあり'},
-      {id: 6, product_status: '傷や汚れあり'},
-      {id: 7, product_status: '全体的に状態が悪い'}
+      {id: 1, product_status: '新品未使用'},
+      {id: 2, product_status: '未使用に近い'},
+      {id: 3, product_status: '目立った傷や汚れなし'},
+      {id: 4, product_status: 'やや傷や汚れあり'},
+      {id: 5, product_status: '傷や汚れあり'},
+      {id: 6, product_status: '全体的に状態が悪い'},
   ]
