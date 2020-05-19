9864ee170229602eec10c381edbc9be011edf607
nemu-meko1019 <gyon0930gyon0315@gmail.com>
2020-05-19 17:12:14 +0900

    カテゴリ実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,8 +6,12 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
-    @item.build_brand
+    # @item.brand.build
     @item.item_images.build
+    @category_parent_array = ["---"]
+    Category.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent.name
+   end
   end
 
   def create
@@ -64,4 +68,11 @@ class ItemsController < ApplicationController
     
   end
 
+  def get_category_children
+    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+  end
+
+  def get_category_grandchildren
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
+  end
 end
