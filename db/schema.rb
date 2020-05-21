275589f20044a4f8e88a36ca632a3f443d76dac4
sho-sun <61076624+sho-sun@users.noreply.github.com>
2020-05-21 19:14:39 +0900

    db関連の修正
    nofileのマイグレーションファイルの処理

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_16_101833) do
+ActiveRecord::Schema.define(version: 2020_05_16_055352) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 2020_05_16_101833) do
     t.integer "price", null: false
     t.bigint "category_id"
     t.bigint "brand_id"
-    t.integer "size_id", null: false
+    t.integer "size_id"
     t.integer "condition_id", null: false
     t.integer "postage_payer_id", null: false
     t.bigint "buyer_id"
@@ -68,8 +68,8 @@ ActiveRecord::Schema.define(version: 2020_05_16_101833) do
     t.timestamp "deal_closed_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "preparation_day_id", null: false
-    t.index ["brand_id"], name: "index_items_on_brand_id"
+    t.string "preparation_day_id", default: "", null: false
+    t.index ["brand_id"], name: "index_items_on_bland_id"
     t.index ["buyer_id"], name: "index_items_on_buyer_id"
     t.index ["category_id"], name: "index_items_on_category_id"
     t.index ["seller_id"], name: "index_items_on_seller_id"
