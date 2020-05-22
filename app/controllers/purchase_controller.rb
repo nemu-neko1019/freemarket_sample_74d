350ca1dcb3269842d0590495575fda4eb7716ad2
kashiwagi1096 <toguro.dayo@gmail.com>
2020-05-22 15:45:14 +0900

    購入機能

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -4,13 +4,13 @@ class PurchaseController < ApplicationController
 
   def index
     @item = Item.find(params[:id])
-    card = Card.where(user_id: current_user.id).first
+    @card = Card.where(user_id: current_user.id).first
     if card = blank?
       redirect_to card_new_path
     else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card_information = customer.cards.retrieve(card.card_id)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
     end
   end
 
@@ -21,14 +21,14 @@ class PurchaseController < ApplicationController
     Payjp::Charge.create(
       :amount => @item.price,
       :customer => card.customer_id,
-      :currency => jpy,
+      :currency => 'jpy',
     )
     redirect_to action: 'done'
   end
 
   def done
     @item = Item.find(params[:id])
-    @item.update(buyer_id: currentuser.id)
+    @item.update(buyer_id: current_user.id)
   end
 
 end
