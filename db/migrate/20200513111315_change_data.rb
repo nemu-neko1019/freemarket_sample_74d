ee3e67a9265f8cdf2ee01d3b4f9f8f0aaa9b3983
takeshi075 <real.feet15@gmail.com>
2020-05-23 12:12:33 +0900

    データーベースリセット

--- db/migrate/20200513111315_change_data.rb
+++
@@ -1,7 +0,0 @@
-class ChangeData < ActiveRecord::Migration[5.2]
-  def change
-    change_column :users, :birthyear, :integer
-    change_column :users, :birthmonth, :integer
-    change_column :users, :birthday, :integer
-  end
-end
