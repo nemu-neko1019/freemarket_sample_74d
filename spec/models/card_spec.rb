2d1805335c0e360383aa09a30827fab1f4a14628
kashiwagi1096 <toguro.dayo@gmail.com>
2020-05-23 11:33:42 +0900

    購入機能（購入画面）

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -2,22 +2,27 @@ require 'rails_helper'
 
 RSpec.describe Card, type: :model do
   describe '#pay' do
+    it "user_id,customer_id,card_idがあれば登録できること" do
+      user = create(:user)
+      card = build(:card, user_id: user.id)
+      expect(card).to be_valid
+    end
+
     it "user_idがない場合は登録できないこと" do
-      card = Card.new(user_id: "", customer_id: "cus_8734c75ee80c80aa11302f03fda5", card_id: "car_0e127d775c0ca15dca27ac5a5ad3")
+      card = build(:card, user_id: "")
       card.valid?
-      expect(user.errors[:user]).to include("can't be blank")
+      expect(card.errors[:user_id]).to include("を入力してください")
     end
 
-    it "customer_idがない場合は登録できないこと" do
-      card = Card.new(user_id: "3", customer_id: "", card_id: "car_0e127d775c0ca15dca27ac5a5ad3")
+    it"customer_idがない場合は登録できないこと" do
+      card = build(:card, customer_id: "")
       card.valid?
-      expect(user.errors[:customer_id]).to include("can't be blank")
+      expect(card.errors[:customer_id]).to include("を入力してください")
     end
-
     it "card_idがない場合は登録できないこと" do
-      card = Card.new(user_id: "3", customer_id: "car_0e127d775c0ca15dca27ac5a5ad3", card_id: "")
+      card = build(:card, card_id: "")
       card.valid?
-      expect(user.errors[:customer_id]).to include("can't be blank")
+      expect(card.errors[:card_id]).to include("を入力してください")
     end
   end
 end
