d482ad6c22e5d9ba4b46d48ab9d2b9a96268788d
sho-sun <61076624+sho-sun@users.noreply.github.com>
2020-05-24 14:49:07 +0900

    ルーティング修正
    _destroyBtn.html.hamlの
    link_toで記述したメソッドの
    ルーティングがdestroyになる様に、
    Application.js内の記述を変更。
    
    コントローラーも修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,9 +26,13 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if item.user_id == current_user.id
-      item.destroy
+    # @item = Item.find(params[:id])
+    # @item.destroy
+    # redirect_to("/")
+    @item = Item.find(params[:id])
+    if @item.seller_id == current_user.id
+      @item.destroy
+      redirect_to root_path
     end
   end
 
--- db/migrate/20200511045506_create_items.rb
+++ db/migrate/20200511045506_create_items.rb
@@ -6,7 +6,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.integer :price, null: false
       t.references :category, foreign_key: true
       t.references :brand, foreign_key: true
-      t.integer :size, null: true
+      t.integer :size_id, null: true
       t.integer :condition_id, null: false
       t.integer :postage_payer_id, null: false
       t.references :buyer, foreign_key: { to_table: :users }, null: true
