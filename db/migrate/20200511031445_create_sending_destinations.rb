b47053c60e11a1a08007b08e03a8741768bbad34
kashiwagi1096 <toguro.dayo@gmail.com>
2020-05-24 11:40:06 +0900

    Update 20200511031445_create_sending_destinations.rb

--- db/migrate/20200511031445_create_sending_destinations.rb
+++ db/migrate/20200511031445_create_sending_destinations.rb
@@ -1,15 +1,12 @@
 class CreateSendingDestinations < ActiveRecord::Migration[5.2]
   def change
     create_table :sending_destinations do |t|
-      t.string :firstname, null: false
-      t.string :familyname, null: false
-      t.string :firstname_kana, null: false
-      t.string :familyname_kana, null: false
+      t.integer :postalcode, null: false
       t.integer :prefecture_id, null: false
       t.string :municipal_district, null: false
       t.string :housenumber, null: false
       t.string :buildingname
-      t.integer :phonenumber, unique: true, null: false
+      t.string :phonenumber, unique: true, null: false
       t.references :user, null: false, foreign_key: true
       t.timestamps
     end
