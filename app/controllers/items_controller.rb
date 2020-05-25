239c6e3a908a1b02d98e519bc85d9ac320c5edad
iba83 <sson.joer@icloud.com>
2020-05-25 10:54:11 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item
+  before_action :set_item, only: [:show, :destroy, :edit, :update]
   def index
     @items = Item.all.order(created_at: :desc)
   end
