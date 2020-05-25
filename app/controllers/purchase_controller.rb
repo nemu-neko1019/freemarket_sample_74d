874639576167aa608e2b6f6aec44907a8eca3534
kashiwagi1096 <toguro.dayo@gmail.com>
2020-05-25 19:25:17 +0900

    Update purchase_controller.rb

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -4,32 +4,40 @@ class PurchaseController < ApplicationController
   require 'payjp'
 
   def index
-    @sending_destination = current_user.sending_destination
-    @card = Card.where(user_id: current_user.id).first
-    if card = blank?
-      redirect_to card_new_path
+    if @item.buyer_id.nil?
+      @sending_destination = current_user.sending_destination
+      @card = Card.where(user_id: current_user.id).first
+      if card = blank?
+        redirect_to card_new_path
+      else
+        Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+        customer = Payjp::Customer.retrieve(@card.customer_id)
+        @default_card_information = customer.cards.retrieve(@card.card_id)
+      end
     else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(@card.customer_id)
-      @default_card_information = customer.cards.retrieve(@card.card_id)
+      redirect_to root_path
     end
   end
 
   def pay
-    card = Card.where(user_id: current_user.id).first
-    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-    Payjp::Charge.create(
-      amount: @item.price,
-      customer: card.customer_id,
-      currency: 'jpy',
-    )
-      redirect_to action: 'done'
+    if @item.buyer_id.nil? && @item.seller_id != current_user.id
+      card = Card.where(user_id: current_user.id).first
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        customer: card.customer_id,
+        currency: 'jpy',
+      )
+        redirect_to action: 'done'
+    else
+      redirect_to root_path
+    end
   end
 
   def done
     @item.update(buyer_id: current_user.id)
     if @item.buyer_id.nil?
-    flash.now[:alert] = 'データの保存に失敗しました'
+      flash.now[:alert] = 'データの保存に失敗しました'
     end
   end
 
